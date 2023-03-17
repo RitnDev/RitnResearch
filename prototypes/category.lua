3e3e13c16f7f5b08c86d72db2cc23239ae453370
RitnDev <ritn@hotmail.fr>
2023-03-17 18:55:33 +0100

    0.8.9 - change item-group : bag & empty-bag

--- prototypes/category.lua
+++ prototypes/category.lua
@@ -2,5 +2,5 @@ local RitnProtoSubgroup = require(ritnlib.defines.class.prototype.subgroup)
 
 --ITEM SUBGROUPS
 RitnProtoSubgroup:extend("ritn-unknow-object", "intermediate-products", "z")
-RitnProtoSubgroup:extend("bag", "intermediate-products", "d")
-RitnProtoSubgroup:extend("empty-bag", "intermediate-products", "d")
+RitnProtoSubgroup:extend("bag", "miner", "t1")
+RitnProtoSubgroup:extend("empty-bag", "miner", "t2")
