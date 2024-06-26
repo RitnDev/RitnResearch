015a36f33293f02e1a0aa44ccf642e8a78194a21
RitnDev <ritn@hotmail.fr>
2024-06-26 04:02:46 +0200

    0.8.10 - migration RitnLib 0.8.2

--- control.lua
+++ control.lua
@@ -1,4 +1,5 @@
 require("__RitnLib__/defines")
+require(ritnlib.defines.setup)
 
 -- gvv
 if script.active_mods["gvv"] then require(ritnlib.defines.gvv)() end
--- modules/disco-science.lua
+++ modules/disco-science.lua
@@ -1,12 +1,12 @@
-local RitnEvent = require(ritnlib.defines.class.luaClass.event)
+-- module.disco-science
 ----------------------------------------------------------------
 local function updateDiscoScience()
-    RitnEvent.setIngredientColor("chemical-science-pack", {r = 0.2, g = 0.2, b = 1.0})
-    RitnEvent.setIngredientColor("military-science-pack", {r = 0.2, g = 0.2, b = 0.2})
-    RitnEvent.setIngredientColor("space-science-pack", {r = 0.8, g = 0.8, b = 0.8})
-    RitnEvent.setIngredientColor("advanced-mechanic-science-pack", {r = 1, g = 0.5, b = 0.0})
-    RitnEvent.setIngredientColor("advanced-electronic-science-pack", {r = 1, g = 1, b = 0.0})
-    RitnEvent.setIngredientColor("advanced-chemical-science-pack", {r = 1, g = 0.0, b = 1})
+    RitnLibEvent.setIngredientColor("chemical-science-pack", {r = 0.2, g = 0.2, b = 1.0})
+    RitnLibEvent.setIngredientColor("military-science-pack", {r = 0.2, g = 0.2, b = 0.2})
+    RitnLibEvent.setIngredientColor("space-science-pack", {r = 0.8, g = 0.8, b = 0.8})
+    RitnLibEvent.setIngredientColor("advanced-mechanic-science-pack", {r = 1, g = 0.5, b = 0.0})
+    RitnLibEvent.setIngredientColor("advanced-electronic-science-pack", {r = 1, g = 1, b = 0.0})
+    RitnLibEvent.setIngredientColor("advanced-chemical-science-pack", {r = 1, g = 0.0, b = 1})
 end
 ----------------------------------------------------------------
 script.on_configuration_changed(updateDiscoScience)
